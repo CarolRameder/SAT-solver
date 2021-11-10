886fb3183e117a4894d8d59dd09b4074b750d595
Rameder Carol <ramederc30@gmail.com>
2021-11-10 21:56:48 +0100

    update

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import time
 DIM = 9
+sudokus="1000_sudokus.txt" #argument
+rules="9x9_sudoku-rules.txt" #read
 
 #returns DIMACS representation of the given input - initial configuration
 def read_game():
-    f = open("1000_sudokus.txt", "r")
+    f = open(sudokus, "r")
     game_rep = f.readline()
     game_final = ""
     for i in range(len(game_rep) - 1):
@@ -14,16 +17,21 @@ def read_game():
 
 
 def read_rules():
-    with open("9x9_sudoku-rules.txt",'r') as f:
+    with open(rules,'r') as f:
         delete=f.readline()
-        rules=f.read()
-    return rules
+        rls=f.read()
+    return rls
 
 def DPLL(sudoku, rules):
+    #print (sudoku + rules)
     pass
 
+start_time = time.time()
+
+print("--- %s seconds ---" % (time.time() - start_time))
 if __name__ == '__main__':
     sudoku = read_game()
     rules = read_rules()
+    start_time = time.time()
     answer = DPLL (sudoku, rules)
 
