f7e1083c446e320e47adb7524d565c45a44c5946
maluknrm <90258947+maluknrm@users.noreply.github.com>
2021-11-13 13:51:46 +0100

    Add files via upload
    new version

--- try_SAT.py
+++ try_SAT.py
@@ -1,6 +1,9 @@
+from functools import lru_cache
+from typing import DefaultDict
 import pycosat
 import sys, getopt
 import time
+from collections import defaultdict
 
 
 
@@ -9,11 +12,14 @@ class DPLL:
     def __init__(self, clause_list, num_list):
         self.clause_list = clause_list
         self.num_list = num_list
+        self.original_dict = {}
+        self.value_dict = {}
+
 
         
 
     # set first dictionary based on the list containing the numbers of the sudoku
-    def set_dict(self):
+    def set_up(self):
 
         for clause in self.clause_list:
             if len(self.clause_list) == 0:
@@ -23,38 +29,71 @@ class DPLL:
 
         
         # set up empty dictionary to put the numbers and values in
-        value_dict = {}
         # set first unit clauses in dict
         for element in self.num_list:
             if element > 0:
-                value_dict[str(element)] = 1
+                self.original_dict[str(element)] = 1
             elif element < 0:
-                value_dict[str(element)[1:]] = 0
+                self.original_dict[str(element)[1:]] = 0
+        
+        # copy original dict to work with it as value dict from now on; leave original dict
+        self.value_dict = self.original_dict.copy()
 
-        return value_dict
-                
+
+    # check for tautologies one time in the beginning         
+    def tautology(self):
+        l = self.clause_list.copy()
+        # loop through clauses
+        for clause in reversed(l):
+            # set taut to false in order to remonve clause if there is a tautology
+            taut = False
+            for number in clause:
+                for i in range(len(clause)):
+                    # if there is a a pos and a neg literal in one clause => remove clause
+                    if -number == clause[i]:
+                        taut  = True    
+            if taut == True:
+                l.remove(clause)
+        return l
+
+    # check for pure literals
+    def pure_literal(self):
+        l = self.clause_list.copy()
+        # set default dic to have all literals saved as keys
+        literal_count = defaultdict(int)
+        for clause in l:
+            for number in clause:
+                literal_count[number] += 1
+        
+        for key in literal_count.keys():
+            # look for pure literals and set them to true or false in value dict 
+            if key and not -key in literal_count.keys():
+                if key > 0:
+                    self.value_dict[str(key)] = 1
+                elif key < 0:
+                    self.value_dict[str(key)] = 0
 
     #based on the first dictionary, loop through clauses again to find unit_clauses and delete then the clauses with one True literal and delete False literals
-    def unit_clauses(self, dictionary):
+    def unit_clauses(self):
         new_list = self.clause_list.copy()
 
         # look if there are unit clauses in clause list
         for clause in self.clause_list:
             if len(clause) == 1:
                 if clause[0] > 0:
-                    dictionary[str(clause[0])] = 1
+                    self.value_dict[str(clause[0])] = 1
                 elif clause[0] < 0:
-                    dictionary[str(clause[0])[1:]] = 0
+                    self.value_dict[str(clause[0])[1:]] = 0
 
-
+        # remove clause if literal is set to true and remove literal from clauses if it is set to false
         for clause in reversed(new_list):
             for number in clause:
-                if str(number) in dictionary:
-                    if dictionary[str(number)] == 1:
+                if str(number) in self.value_dict:
+                    if self.value_dict[str(number)] == 1:
                         new_list.remove(clause)
                         break
                     
-                    elif dictionary[str(number)] == 0:
+                    elif self.value_dict[str(number)] == 0:
                         clause.remove(number)
 
         return new_list
