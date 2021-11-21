6ab20f30d18279f0f8249ac1da2dce75ec226ea9
Rameder Carol <ramederc30@gmail.com>
2021-11-21 15:43:51 +0100

    DLIS & DLCS working

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import numpy as np
 import copy
 import time
 import math
+from collections import defaultdict
 sys.setrecursionlimit(10000)
 #sudokus = "4x4.txt"  # argument in line
-sudokus = "1000_sudokus.txt"
+sudokus = "16x16.txt"
 #sudokus = "1000_sudokus.txt"
 rules_d={
     4:"sudoku-rules-4x4.txt",
@@ -23,9 +24,64 @@ def select_literal(cnf):
         for literal in c:
             return literal
 
+def DLCS(lst):
+    clauses = copy.deepcopy(lst)
+    literal_count = defaultdict(int)
+    literal_count2 = defaultdict(int)
+    for clause in clauses:
+        for number in clause:
+            literal_count[abs(number)] += 1
+            literal_count2[number] += 1
+    max_key = max(literal_count, key=literal_count.get)
+
+    if max_key and -max_key in literal_count2:
+        if literal_count2[max_key] > literal_count2[-max_key]:
+            return max_key
+        else:
+            return -max_key
+    else:
+        return max_key
+
+# we can choose if we take the highest positive or negative value => can decide with True
+def DLIS(lst, key_max=True):
+    clauses = copy.deepcopy(lst)
+    pos_literal_count = defaultdict(int)
+    neg_literal_count = defaultdict(int)
+    for clause in clauses:
+        for number in clause:
+            if number > 0:
+                pos_literal_count[number] += 1
+            else:
+                neg_literal_count[number] += 1
+
+    # pick two max values => one for positive and one for negative literals
+    max_key = max(pos_literal_count, key=pos_literal_count.get)
+    min_key = max(neg_literal_count, key=neg_literal_count.get)
+
+    if key_max == True:
+        if max_key in pos_literal_count and -max_key in neg_literal_count:
+            if pos_literal_count[max_key] > neg_literal_count[-max_key]:
+                return max_key
+            else:
+                return -max_key
+        else:
+            return max_key
+
+    else:
+        if min_key in neg_literal_count and -min_key in pos_literal_count:
+            if neg_literal_count[min_key] > pos_literal_count[-min_key]:
+                return min_key
+            else:
+                return -min_key
+        else:
+            return min_key
+
+
 #2,3 to be added
 heuristics={
-    1:select_literal
+    1:select_literal,
+    2:DLIS,
+    3:DLCS
 }
 
 # returns DIMACS representation of the given input - initial configuration
@@ -146,7 +202,7 @@ def rec(clauses, lit, sol):
 if __name__ == '__main__':
     # unused
     global h
-    h=1
+    h=3
     #h=int(sys.argv[1]) #set heuristc choice
     #input_file=sys.argv[2] #argument for read_game()
     #
